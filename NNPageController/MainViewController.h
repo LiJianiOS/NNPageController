514094fa375bb8e5c13271fa4fa905ad06c2d382
public <niuting604@163.com>
2015-10-30 11:00:58 +0800

    只是草稿，实现部分功能

---
+++ NNPageController/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  NNPageController
+//
+//  Created by public on 15/10/30.
+//  Copyright © 2015年 public. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ NNPageController/NNPageController/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  NNPageController
+//
+//  Created by public on 15/10/30.
+//  Copyright © 2015年 public. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
